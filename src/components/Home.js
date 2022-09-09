b623b433eb9b6b64bfc8fcf1159793d198f9bb19
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-10 00:00:16 +0600

    filters added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useContext } from "react";
+import { Cart } from "../context/Context";
+import SingleProduct from "./SingleProduct";
+import "./styles.css";
+import Filters from "./Filters";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    state: { products },
+    dispatch,
+  } = useContext(Cart);
+  // console.log(products);
+  return (
+    <div className="home">
+      <Filters />
+      <div className="productContainer">
+        {products.map((prod) => {
+          return <SingleProduct prod={prod} key={prod.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/context/Context.js
+++ src/context/Context.js
@@ -1,7 +1,9 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { faker } from "@faker-js/faker";
+import cartReducer from "./Reducers";
+import React from "react";
 
-const Cart = createContext();
+export const Cart = React.createContext();
 const Context = ({ children }) => {
   const products = [...Array(20)].map(() => ({
     id: faker.datatype.uuid(),
@@ -12,12 +14,21 @@ const Context = ({ children }) => {
     fastDelivery: faker.datatype.boolean(),
     ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
   }));
-  console.log(products);
+  const initialState = {
+    products: products,
+    cart: [],
+  };
+  // console.log(products);
+  const [state, dispatch] = useReducer(cartReducer, initialState);
   return (
     <>
-      <Cart.Provider>{children}</Cart.Provider>
+      <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>
     </>
   );
 };
 
 export default Context;
+
+// export const CartState = () => {
+//   return useContext(Cart);
+// };
