62d05ff3c41819fb254c5cfec7495c9ffd498847
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-09 23:00:00 +0600

    added faker and all the hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import {
-  Badge,
-  Container,
-  FormControl,
-  Nav,
-  Navbar,
-  NavbarBrand,
-} from "react-bootstrap";
-import { FaShoppingCart } from "react-icons/fa";
 import Header from "./components/Header";
-import "./App.css";
-import Dropdown from "react-bootstrap/Dropdown";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
-        <Container>
-          <Navbar.Brand>
-            <a href="#">Shopping Cart</a>
-          </Navbar.Brand>
-          <Navbar.Text className="search">
-            <FormControl
-              style={{ width: 500 }}
-              placeholder="Search for product"
-              className="m-auto"
-            />
-          </Navbar.Text>
-          <Nav>
-            <Dropdown alignRight>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                <FaShoppingCart color="white" fontSize="25px" />
-                {/* <Badge style={{ fontSize: 5 }}>{10}</Badge> */}
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu style={{ minWidth: 370 }}>
-                <span style={{ padding: 5 }}>Cart is empty!</span>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/cart" exact element={<Cart />} />
+      </Routes>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
-import React from "react";
-
+import { Badge, Container, FormControl, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import "../App.css";
+import Dropdown from "react-bootstrap/Dropdown";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">Shopping Cart</Link>
+          </Navbar.Brand>
+          <Navbar.Text className="search">
+            <FormControl
+              style={{ width: 500 }}
+              placeholder="Search for product"
+              className="m-auto"
+            />
+          </Navbar.Text>
+          <Nav>
+            <Dropdown alignRight>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <FaShoppingCart color="white" fontSize="25px" />
+                {/* <Badge style={{ fontSize: 5 }}>{10}</Badge> */}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu style={{ minWidth: 370 }}>
+                <span style={{ padding: 5 }}>Cart is empty!</span>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
